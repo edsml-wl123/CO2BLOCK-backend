98e429579702aa1dca3bdd35ce716d0b4379c783
scywl3 <scywl3@nottingham.edu.cn>
2024-07-26 17:02:02 +0800

    set cors

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,10 @@ from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
     CORS(app)  # Enable CORS for all routes
+    CORS(app, resources={r"/*": {"origins": "13.55.134.49"}})  # Allow all origins
 
     # Register routes
     from app.routes import bp as routes_bp
-    app.register_blueprint(routes_bp)
+    app.register_blueprint(routes_bp,url_prefix='/api')
 
     return app
